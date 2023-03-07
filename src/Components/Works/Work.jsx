80c4b115cc0da104cffa281ce2d7c0120ca87ec1
mauroarnedo <mauro.arnedo7@gmail.com>
2023-03-06 22:27:17 -0300

    Edit view on carousel and works

--- src/Components/Works/Work.jsx
+++ src/Components/Works/Work.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { Button, Card, Modal } from "react-bootstrap";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const Work = ({ work }) => {
     const [show, setShow] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleCloseInfo = () => setShowInfo(false);
+    const handleShowInfo = () => setShowInfo(true);
 
     return (
         <div className="frame">
@@ -16,24 +20,42 @@ const Work = ({ work }) => {
                 onClick={handleShow}
             />
 
-            <Modal show={show} onHide={handleClose} style={{ color: "black"}}>
-                <Modal.Header closeButton>
-                    <Modal.Title>{work.title}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Año: {work.year} <br />
-                    Técnica: {work.technique} <br />
-                    Medidas: {work.measure} <br />
-                    Precio: {work.price} <br />
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button href={work.image} target="_blank">
-                        Ver imagen completa
-                    </Button>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
+            <Modal show={show} fullscreen={true} onHide={handleClose}>
+                <TransformWrapper
+                    // initialScale={1}
+                    initialPositionX={200}
+                    initialPositionY={100}
+                >
+                    {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                        <>
+                            <div className="tools">
+                                <Button variant="dark" onClick={() => zoomIn()}>ZOOM IN +</Button>
+                                <Button variant="dark" onClick={() => zoomOut()}>ZOOM OUT-</Button>
+                                <Button variant="dark" onClick={() => resetTransform()}>RESET</Button>
+                                <Button variant="secondary" onClick={handleClose}>Close</Button>
+                                <Button onClick={handleShowInfo}>Descripcion</Button>
+                            </div>
+                            <TransformComponent>
+                                <img src={work.image} alt={work.title} style={{ width: "100%", height: "100%" }} />
+                                <div>
+                                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                                    <Button onClick={handleShowInfo}>Descripcion</Button>
+                                    <Modal show={showInfo} onHide={handleCloseInfo} style={{ color: "black" }}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>{work.title}</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            Año: {work.year} <br />
+                                            Técnica: {work.technique} <br />
+                                            Medidas: {work.measure} <br />
+                                            Precio: {work.price} <br />
+                                        </Modal.Body>
+                                    </Modal>
+                                </div>
+                            </TransformComponent>
+                        </>
+                    )}
+                </TransformWrapper>
             </Modal>
         </div>
     )
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useRef, useEffect } from "react";
-import Glide from "@glidejs/glide";
 import { Context } from "../../Context/context";
 import './Works.css';
 import Work from "./Work";
@@ -7,28 +6,33 @@ import Work from "./Work";
 export default function Works() {
   const { works } = useContext(Context)
 
-  const worksRef = useRef(null)
-  useEffect(() => {
-    if (works.length) {
-      new Glide(worksRef.current, {
-        type: "carousel",
-        perView: 1,
-        autoplay: 3000,
-      }).mount();
-    }
-  }, [works.length]);
+  // const worksRef = useRef(null)
+  // useEffect(() => {
+  //   if (works.length) {
+  //     new Glide(worksRef.current, {
+  //       type: "carousel",
+  //       perView: 1,
+  //       autoplay: 3000,
+  //     }).mount();
+  //   }
+  // }, [works.length]);
 
   return (
-    <div className="glide w-50" ref={worksRef}>
-      <div className="glide__track" data-glide-el="track">
-        <ul className="glide__slides flex align-items-center">
-          {works.map((work, index) => (
-            <li className="glide__slide" key={index}>
-              <Work work={work} />
-            </li>
-          ))}
-        </ul>
-      </div>
+    // <div className="glide w-50" ref={worksRef}>
+    //   <div className="glide__track" data-glide-el="track">
+    //     <ul className="glide__slides flex align-items-center">
+    //       {works.map((work, index) => (
+    //         <li className="glide__slide" key={index}>
+    //           <Work work={work} />
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </div>
+    <div className="works">
+      {works.map((work, index) => (
+        <Work key={index} work={work} />
+      ))}
     </div>
   );
 }
