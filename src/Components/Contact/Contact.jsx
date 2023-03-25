6d957fbd77245d05e8daa9581aaca12713e46cbd
mauroarnedo <mauro.arnedo7@gmail.com>
2023-03-25 17:49:43 -0300

    fix web style

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,10 +6,10 @@ const Contact = () => {
         <div className="contact-container">
             <h2>Contacto</h2>
             <p>
-            Bienvenido/a a mi sección de contacto. Estoy emocionado/a de tener la oportunidad de conectarme contigo. <br/>
+            Bienvenido/a a mi sección de contacto. Estoy interesado de tener la oportunidad de conectarme contigo. <br/>
             Si tienes alguna pregunta, comentario o simplemente deseas saludar, no dudes en ponerte en contacto conmigo. <br/>
             Ya sea que desees comentar sobre mis obras de arte, hacer una pregunta acerca de una exposición o simplemente <br/>
-            compartir tus pensamientos sobre el mundo del arte, siempre estoy dispuesto/a a escuchar. <br/>
+            compartir tus pensamientos sobre el mundo del arte, siempre estoy dispuesto a escuchar. <br/>
             Espero tener noticias tuyas pronto y gracias por tomarte el tiempo de visitar mi sitio web.
             </p>
             <span><strong>danielleonardoarnedo@gmail.com</strong></span>
